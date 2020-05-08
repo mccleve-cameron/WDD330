de90edf3a328aa1d5b3a0578a20532655c6526aa
mccleve-cameron <39345347+mccleve-cameron@users.noreply.github.com>
2020-05-08 00:33:15 -0600

    week2 notes

---
+++ week1/week1.js
@@ -0,0 +1,9 @@
+/* local storage */
+localStorage.setItem("firstName", "Cameron");
+localStorage.lastName = 'McCleve';
+
+let firstName = localStorage.getItem('firstName');
+let lastName = localStorage.lastName;
+
+console.log(firstName);
+console.log(lastName);
